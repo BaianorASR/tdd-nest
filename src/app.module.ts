f3c36e5d4a808a09e99feeec9b5da61df1687a2b
BaianorASR <reis.adson2@outlook.com>
2022-11-26 12:32:23 -0300

    feat: setup husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import * as bcrypt from 'bcrypt';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column('varchar', { length: 120 })
+  username: string;
+
+  @Column('varchar', { length: 30, select: false })
+  password: string;
+
+  @BeforeInsert()
+  private async hashPassword(): Promise<void> {
+    const saltOrRounds = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, saltOrRounds);
+  }
+}
