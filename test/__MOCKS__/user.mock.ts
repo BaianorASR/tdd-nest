850b82811ba750836fc7f50182f908bc3106f289
BaianorASR <reis.adson2@outlook.com>
2022-11-27 15:47:15 -0300

    fix: mocks use plainToInstance

--- test/__MOCKS__/user.mock.ts
+++ test/__MOCKS__/user.mock.ts
@@ -1,12 +1,16 @@
+import { plainToInstance } from 'class-transformer';
+
 import { User } from '@entities/user.entity';
 
-export const Mock_User = {
+import { CreateUserDto } from '../../src/modules/user/dto/create-user.dto';
+
+export const Mock_User = plainToInstance(User, {
   id: 1,
   username: 'test',
   password: 'test',
-} as User;
+});
 
-export const Mock_Create_User_Dto = {
+export const Mock_Create_User_Dto = plainToInstance(CreateUserDto, {
   username: 'test',
   password: 'test',
-};
+});
